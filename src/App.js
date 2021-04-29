25c6eaf91162106c097a529f36993a695e3a0d2d
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-04-29 14:54:22 +0900

    Add Todos Container redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
-import Todos from "./components/Todos";
 import CounterContainer from "./containers/CounterContainer";
+import TodosContainer from "./containers/TodosContainer";
 
 function App() {
   return (
     <div>
       <CounterContainer />
       <hr />
-      <Todos />
+      <TodosContainer />
     </div>
   );
 }
