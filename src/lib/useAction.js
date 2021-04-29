202d040329b0930e12c0e939bfc5c9ae622d8c10
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-04-29 16:45:12 +0900

    use useSelector and custom hooks as useActions

---
+++ src/lib/useAction.js
@@ -0,0 +1,17 @@
+import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+
+export default function useActions(actions, deps) {
+  const dispatch = useDispatch();
+  return useMemo(
+    () => {
+      if (Array.isArray(actions)) {
+        return actions.map((action) => bindActionCreators(action, dispatch));
+      }
+      return bindActionCreators(actions, dispatch);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    deps ? [dispatch, ...deps] : deps
+  );
+}
